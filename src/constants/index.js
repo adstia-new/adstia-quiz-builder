2847c6cc726fd24021a94368aa0f9752ef341b74
Ashutosh Sharma <ashutoshsharma.120599@gmail.com>
2026-02-06 20:48:55 +0530

    Added _fbp & _fbc in quiz module submission

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,8 @@ export const LOCAL_STORAGE_QUIZ_VALUES = 'quizValues';
 export const LOCAL_STORAGE_QUIZ_HISTORY = 'quizHistory';
 export const SESSION_STORAGE_SESSION_ID_KEY = 'session_id';
 export const COOKIE_ANONYMOUS_ID = '__eventn_id';
+export const COOKIE_FBP = '_fbp';
+export const COOKIE_FBC = '_fbc';
 export const ZIPCODE_API_URL = 'https://server.adstiacms.com/api/zipcode/us';
 export const QUIZ_MODULE_SUBMISSION_URL =
   'https://server.adstiacms.com/v2/api/save-quiz-module-submission';
